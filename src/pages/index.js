2c79da32c63b31be223b65481ce824b2afbf1c76
erayerogul <erayerogul@gmail.com>
2023-05-08 14:39:09 +0300

    Created styles.main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Index() {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,29 +15,28 @@ export default function Home() {
       </Head>
       <header className={styles.main}>
         <div>
-          <img src="https://cdn.raatbodrum.com/logo/logo.png" width="160" height="160" draggable="false" class="sc-gsTEea jQLxmr" />
+          <img src="https://cdn.raatbodrum.com/logo/logo.png" width="160" height="160" draggable="false" />
         </div>
       </header>
-     
       <main className={styles.main}>
         <div>
           <a href='/beer'>
-          <img src="https://cdn.raatbodrum.com/buttons/beer.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/beer.png" width="100" draggable="false"/>
           </a>
           <a href='/whiskey'>
-          <img src="https://cdn.raatbodrum.com/buttons/whiskey.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/whiskey.png" width="100" draggable="false"/>
           </a>
           <a href='/wine'>
-          <img src="https://cdn.raatbodrum.com/buttons/wine.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/wine.png" width="100" draggable="false"/>
           </a>
           <a href='/cocktails'>
-          <img src="https://cdn.raatbodrum.com/buttons/cocktails.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/cocktails.png" width="100" draggable="false"/>
           </a>
           <a href='/coffee'>
-          <img src="https://cdn.raatbodrum.com/buttons/coffee.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/coffee.png" width="100" draggable="false"/>
           </a>
           <a href='/tea'>
-          <img src="https://cdn.raatbodrum.com/buttons/tea.png" width="100" draggable="false" class="sc-gsTEea jQLxmr" />
+            <img src="https://cdn.raatbodrum.com/buttons/tea.png" width="100" draggable="false"/>
           </a>
         </div>
       </main>
